d1af2639297fce8fd417903e38a1592a70fca5f6
Luigi Vitali <primepromanagment@gmail.com>
2026-02-23 19:26:07 +0000

    File changes

--- src/components/ui/dialog.jsx
+++ src/components/ui/dialog.jsx
@@ -25,7 +25,7 @@ const DialogOverlay = React.forwardRef(({ className, ...props }, ref) => (
 ))
 DialogOverlay.displayName = DialogPrimitive.Overlay.displayName
 
-const DialogContent = React.forwardRef(({ className, children, ...props }, ref) => (
+const DialogContent = React.forwardRef(({ className, children, style, ...props }, ref) => (
   <DialogPortal>
     <DialogOverlay />
     <DialogPrimitive.Content
@@ -34,6 +34,12 @@ const DialogContent = React.forwardRef(({ className, children, ...props }, ref) 
         "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg",
         className
       )}
+      style={{
+        background: '#0a1120',
+        borderColor: 'rgba(99,102,241,0.2)',
+        color: '#e2e8f0',
+        ...style,
+      }}
       {...props}>
       {children}
       <DialogPrimitive.Close
